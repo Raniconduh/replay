e165c4d316c487a5a558d8c67e77e0b202c6e178
Raniconduh <clagv.randomgames@gmail.com>
2020-12-01 20:02:53 -0600

    Added ability to use items from inventory

--- src/replay.c
+++ src/replay.c
@@ -101,17 +101,79 @@ void printHeader(char * menuName) {
 
 
 int invMenu() {
+invLabel:
 	printHeader("Inventory");
 
-	for (int i = 0; i < arrlen(inventoryOptions); i++) {
-		printf("%s: ", inventoryOptions[i]);
+	for (size_t i = 0; i < arrlen(inventoryOptions); i++) {
+		printf("%s  %ld. %s%s: ", CYAN, i + 1, RESET, inventoryOptions[i]);
 		printf("%s%ld%s\n", YELLOW, inventoryCount[i], RESET);
 	}
 
-	printf("\n%sPress Enter to Continue%s\n", YELLOW, RESET);
+	printf("%s  %ld. Exit%s\n\n", CYAN, arrlen(inventoryOptions) + 1, RESET);
 
-	while (getchar() != '\n');
-	getchar();
+	printf("%s>>>%s ", YELLOW, RESET);
+	char * userInput = malloc(3);
+	scanf("%s", userInput);
+
+	char * exitNumber = malloc(arrlen(inventoryCount) * sizeof(int));
+	sprintf(exitNumber, "%ld", arrlen(inventoryCount) + 1);
+
+	printf("%s  %s. Exit%s\n\n", CYAN, exitNumber, RESET);
+
+	// if the exit option is chosen
+	if (!strcmp(userInput, exitNumber)) {
+		return 0;
+
+	// if option 1 is chosen
+	} else if (!strcmp(userInput, "1")) {
+
+		// If there are more than 0 health potions and option 1 is chosen
+		if (inventoryCount[0] > 0)  {
+
+			// If current health is far enough away from max health
+			if (character.health < character.totalHealth - 15) {
+			printf("%s%sYou used %s1%s Health Potion and gained %s15%s Health.%s\n",
+					CLEAR, PURPLE,
+					YELLOW, PURPLE, YELLOW, PURPLE,
+					RESET);
+			
+			inventoryCount[0]--;
+			character.health += 15;
+			
+			sleep(1);
+			
+			// if current health is too close to max health
+			} else {
+				printf("%s%sYou cannot use this item. Health is too high%s\n",
+						CLEAR, PURPLE, RESET);
+				sleep(1);
+
+			}
+
+		// If there are not enough health potions
+		} else {
+			printf("%s%sYou do not have any Health Potions%s\n",
+					CLEAR, PURPLE, RESET);
+			sleep(1);
+		}
+	
+		goto invLabel;
+
+	// If chosen option is not a usable item or does not exist
+	} else {
+		free(userInput);
+		free(exitNumber);
+
+		printf("%s%sYou cannot use this item.%s\n",
+				CLEAR, PURPLE, RESET);
+
+		sleep(1);
+
+		goto invLabel;
+	}
+
+	free(userInput);
+	free(exitNumber);
 
 	return 0;
 };
