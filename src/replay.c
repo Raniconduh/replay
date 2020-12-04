0775d5abbf8c0303fec39efc3ed6fcbf2fe42134
Raniconduh <clagv.randomgames@gmail.com>
2020-12-03 19:38:44 -0600

    Added load save menu (saves cannot yet be loaded)

--- src/replay.c
+++ src/replay.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <dirent.h>
+#include <errno.h>
 
 
 #define arrlen(array) sizeof(array) / sizeof(array[0])
@@ -16,6 +18,7 @@ int dungeon();
 int shop();
 int invMenu();
 void printHeader(char * menuName);
+int startMenu();
 
 
 const char CLRLINE[] = "\033[2K\r";
@@ -76,11 +79,107 @@ size_t inventoryCount[] = {2, 0};
 
 
 int main() {
+	startMenu();
+
 	makeCharacter();
 
 	character.coins = 15;
 
 	mainMenu();
+	return 0;
+}
+
+
+int startMenu() {
+startMenuLabel:
+	printf("%s%sWelcome to Replay!%s\n\n", CLEAR, GREEN, RESET);
+
+	printf("%s  1. Load Save\n", CYAN);
+	printf("  2. Create New Save%s\n\n", RESET);
+
+	printf("%s>>>%s ", YELLOW, RESET);
+	char * userInput = malloc(3);
+	scanf("%s", userInput);
+
+	// Load an existing save
+	if (!strncmp(userInput, "1", 1)) {
+		char * home = getenv("HOME");
+		strcat(home, "/.config/replay");
+
+		DIR * dir = opendir(home);
+		struct dirent * dirContents;
+		size_t saves = 0;
+		char ** saveFiles = malloc(sizeof(char) * saves + 1);
+
+		// If directory exists
+		if (dir) {
+			while ((dirContents = readdir(dir)) != NULL) {
+				// storing contents of directory
+
+				// if file name is not . nor ..
+				if (strcmp(dirContents->d_name, ".") && strcmp(dirContents->d_name, "..")) {
+					saveFiles[saves] = dirContents->d_name;
+					saves++;
+					void * ptr = realloc(saveFiles, sizeof(char) * saves + 1);
+				}
+					
+			}
+
+			if (saves == 0) {
+				printf("%s%sNo saves found.%s\n", CLEAR, PURPLE, RESET);
+				sleep(1);
+
+				closedir(dir);
+
+				goto startMenuLabel;
+			}
+
+			printf("%s%sSelect a Save%s\n\n", CLEAR, GREEN, RESET);
+		
+			size_t i;
+			for (i = 1; i <= saves; i++) {
+				printf("%s  %ld. %s%s\n", CYAN, i, saveFiles[i-1], RESET);
+			}
+
+			printf("%s  %ld. Exit%s\n\n", CYAN, i, RESET);
+
+			printf("%s>>>%s ", YELLOW, RESET);
+			scanf("%s", userInput);
+			
+			char * stringi = malloc(sizeof(i));
+			snprintf(stringi, sizeof(i), "%ld", i);
+
+			if (!strncmp(userInput, stringi, 1)) {
+				home = NULL;
+				dir = NULL;
+				dirContents = NULL;
+				startMenu();
+			}
+			
+			free(stringi);
+			free(saveFiles);
+			closedir(dir);
+
+		// Directory does not exist
+		} else if (ENOENT == errno) {
+			printf("%s%sNo directory found.%s\n", CLEAR, RED, RESET);
+			sleep(1);
+
+		// opendir failed
+		} else {
+			printf("%s%sProblem searching for saves.%s\n", CLEAR, RED, RESET);
+			sleep(1);
+			
+			goto startMenuLabel;
+		}
+
+	// Make a new save
+	} else if (!strncmp(userInput, "2", 1)) {
+		
+	}
+
+	free(userInput);
+
 	return 0;
 }
 
