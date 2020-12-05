336acfea5a0124c46d03d47f18d7d05c598f0ce0
Raniconduh <clagv.randomgames@gmail.com>
2020-12-04 21:18:21 -0600

    removed variable definitions and replaced them with extern declarations

--- src/replay.h
+++ src/replay.h
@@ -2,7 +2,7 @@
 
 
 // user struct
-typedef struct {
+struct pc {
 	// user chosen username
 	char * name;
 	// user chosen class (Warrior, Mage, or Archer)
@@ -15,48 +15,29 @@ typedef struct {
 	size_t health;
 	size_t damage;
 	size_t coins;
-} playerCharacter;
-
+};
+typedef struct pc playerCharacter;
 
 
 int makeCharacter();
 int mainMenu();
 int dungeon();
 int shop();
-int invMenu();
+int invMenu(playerCharacter * character);
 void printHeader(char * menuName);
 int startMenu();
 
-const char CLRLINE[] = "\033[2K\r";
-const char RESET[] = "\033[0;0m";
-const char BLACK[] = "\033[0;30m";
-const char RED[] = "\033[0;31m";
-const char GREEN[] = "\033[0;32m";
-const char YELLOW[] = "\033[0;33m";
-const char BLUE[] = "\033[0;34m";
-const char PURPLE[] = "\033[0;35m";
-const char CYAN[] = "\033[0;36m";
-const char WHITE[] = "\033[0;37m";
-const char CLEAR[] = "\033[H\033[2J";
+extern const char * CLRLINE;
+extern const char * RESET;
+extern const char * BLACK;
+extern const char * RED;
+extern const char * GREEN;
+extern const char * YELLOW;
+extern const char * BLUE;
+extern const char * PURPLE;
+extern const char * CYAN;
+extern const char * CLEAR;
 
-const char HBLACK[] = "\033[0;40m";
-const char HRED[] = "\033[0;41m";
-const char HGREEN[] = "\033[0;42m";
-const char HYELLOW[] = "\033[0;43m";
-const char HBLUE[] = "\033[0;44m";
-const char HPURPLE[] = "\033[0;45m";
-const char HCYAN[] = "\033[0;46m";
-const char HWHITE[] = "\033[0;47m";
-
-// Sizes of enemies (Tiny, Small, Normal Sized, Large, Enormously big)
-char* sizes[] = {"Tiny", "Small", "Normal Sized", "Large", "Enormously Big"};
-
-// All possible enemies
-char * enemies[] = {
- 	"Witch", "Goblin", "Ogre", "Vampire", "Ghoul", "Wolf",
-	"Mummy", "Zombie", "Ghost", "Troll", "Giant", "Scorpion", 
-	"Warlock", "Ant Colony", "Necromancer", "Centipede"
-};
 
 // enemy struct
 typedef struct {
@@ -87,7 +68,10 @@ typedef struct {
 
 
 // all possible items (Health Potion, Flesh)
-char * inventoryOptions[] = {"Health Potion", "Flesh"};
+extern char * inventoryOptions[2];
 // amount in inventory (Health Potion, Flesh)
-size_t inventoryCount[] = {2, 0};
+extern size_t inventoryCount[2];
+
+extern char * sizes[5];
+extern char * enemies[16];
 
